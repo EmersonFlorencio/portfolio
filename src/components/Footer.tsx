aa3bba546a33d4c95ae18a3fcb08393e1269f64a
Emerson Florencio <emersonflorencio@hotmail.com>
2023-07-20 14:19:27 -0300

    style: finalizando a pagina home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,28 @@
 "use client"
 
 import { styled } from "styled-components"
+import Link from "next/link"
+import { GithubIcon } from "./icons/github-icon"
+import { LinkedinIcon } from "./icons/linkedin-icon"
 
 const FooterContainer = styled.footer`
     color: white;
     background-color: #231942;
     display: flex;
     justify-content: center;
-    gap: 20px;
+    gap: 30px;
     height: 50px;
     align-items: center;
     font-size: 24px;
+
+    > a {
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+    font-size: 24px;
+    font-weight: 600;
+    display: flex;
+  } 
 `
 const DivContainer = styled.div`
   color: white;
@@ -28,8 +40,14 @@ export default function Footer() {
   return (
     <>
     <FooterContainer>
-        <p>GitHub</p>
-        <p>Linkedin</p>
+        <Link href={'https://github.com/EmersonFlorencio'} target="_blank">
+          <GithubIcon />
+          GitHub
+        </Link>
+        <Link href={'https://www.linkedin.com/in/emerson-florencio-dev/'} target="_blank">
+          <LinkedinIcon />
+          Linkedin
+        </Link>
     </FooterContainer>
       <DivContainer>
         <p>Desenvolvido Por Emerson Florencio</p>
